110f400ea7f532bd6794a8f88152ccfa7e3155e3
Jo0Yo0n <dlwndbs10@gmail.com>
2024-07-23 00:09:43 +0900

    📱 Design: Header 디자인 변경

--- src/components/common/TheHeader.vue
+++ src/components/common/TheHeader.vue
@@ -1,23 +1,35 @@
 <template>
     <v-app-bar :elevation="3">
-        <template v-slot:prepend>
-            <v-app-bar-nav-icon></v-app-bar-nav-icon>
-        </template>
+        <v-container class="pa-0 fill-height">
+            <v-row no-gutters align="center" justify="space-between">
+                <v-app-bar-title class="text-h6 font-weight-bold mr-10">Drop The Bid</v-app-bar-title>
 
-        <v-app-bar-title>Drop The Bid</v-app-bar-title>
+                <v-btn class="ml-4">
+                    <b>Category</b>
+                    <v-icon right>mdi-chevron-down</v-icon>
+                </v-btn>
 
-        <v-button>
-            <b>Category</b>
-            <v-icon big>mdi-chevron-down</v-icon>
-        </v-button>
+                <v-spacer></v-spacer>
 
-        <template v-slot:append>
-            <v-btn icon="mdi-heart"></v-btn>
+                <v-col cols="auto">
+                    <v-row no-gutters align="center">
+                        <v-text-field label="검색" append-icon="mdi-magnify" hide-details outlined dense class="mr-4" style="max-width: 300px"></v-text-field>
 
-            <v-btn icon="mdi-magnify"></v-btn>
-
-            <v-btn icon="mdi-dots-vertical"></v-btn>
-        </template>
+                        <v-btn icon class="mx-1">
+                            <v-icon>mdi-chat-outline</v-icon>
+                        </v-btn>
+                        <v-btn icon class="mx-1">
+                            <v-icon>mdi-heart-outline</v-icon>
+                        </v-btn>
+                        <v-btn icon class="mx-1">
+                            <v-icon>mdi-account-circle-outline</v-icon>
+                        </v-btn>
+                        <v-btn class="ml-2" rounded="xl" dark>Login</v-btn>
+                        <v-btn class="ml-2" rounded="xl" dark>Sign Up</v-btn>
+                    </v-row>
+                </v-col>
+            </v-row>
+        </v-container>
     </v-app-bar>
 </template>
 
