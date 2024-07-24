b49e22c9008cb27512626f87308e426353e41069
JooYoon <dlwndbs10@gmail.com>
2024-07-24 11:40:11 +0900

    :iphone: Design: 헤더 디자인 수정

--- src/components/common/TheHeader.vue
+++ src/components/common/TheHeader.vue
@@ -2,38 +2,33 @@
 <!-- 작성자: 이주윤 -->
 
 <template>
-    <v-app-bar :elevation="3">
-        <v-container class="pa-0 fill-height">
-            <v-row no-gutters align="center" justify="space-between">
-                <v-app-bar-title class="text-h6 font-weight-bold mr-10">Drop The Bid</v-app-bar-title>
+    <v-app-bar app flat>
+        <v-img src="@/assets/team-logo.png" max-height="60" max-width="200" contain class="mr-10"></v-img>
 
-                <v-btn class="ml-4">
-                    <b>Category</b>
-                    <v-icon right>mdi-chevron-down</v-icon>
-                </v-btn>
+        <v-btn text>
+            Category
+            <v-icon>mdi-chevron-down</v-icon>
+        </v-btn>
 
-                <v-spacer></v-spacer>
+        <v-spacer></v-spacer>
 
-                <v-col cols="auto">
-                    <v-row no-gutters align="center">
-                        <v-text-field label="검색" append-icon="mdi-magnify" hide-details outlined dense class="mr-4" style="max-width: 300px"></v-text-field>
+        <v-text-field label="검색" hide-details="true" append-icon="mdi-magnify" style="max-width: 300px"></v-text-field>
 
-                        <v-btn icon class="mx-1">
-                            <v-icon>mdi-chat-outline</v-icon>
-                        </v-btn>
-                        <v-btn icon class="mx-1">
-                            <v-icon>mdi-heart-outline</v-icon>
-                        </v-btn>
-                        <v-btn icon class="mx-1">
-                            <v-icon>mdi-account-circle-outline</v-icon>
-                        </v-btn>
-                        <v-btn class="ml-2" rounded="xl" dark>Login</v-btn>
-                        <v-btn class="ml-2" rounded="xl" dark>Sign Up</v-btn>
-                        <v-btn class="ml-2" rounded="xl" dark>Logout</v-btn>
-                    </v-row>
-                </v-col>
-            </v-row>
-        </v-container>
+        <v-btn icon class="ml-4">
+            <v-icon>mdi-chat-outline</v-icon>
+        </v-btn>
+
+        <v-btn icon class="ml-4">
+            <v-icon>mdi-heart-outline</v-icon>
+        </v-btn>
+
+        <v-btn icon class="ml-4">
+            <v-icon>mdi-account-circle-outline</v-icon>
+        </v-btn>
+
+        <v-btn class="ml-4" dark rounded> Login </v-btn>
+        <v-btn class="ml-4" dark rounded> Logout </v-btn>
+        <v-btn class="ml-4" dark rounded> Sign in </v-btn>
     </v-app-bar>
 </template>
 
