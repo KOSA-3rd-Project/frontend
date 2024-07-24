c819653b22c67b2b959bb33a8ead9f186d87088d
JaeeunCho <redjoun@gmail.com>
2024-07-24 21:33:39 +0900

    Feat websocket front

--- src/router/route.js
+++ src/router/route.js
@@ -1,9 +1,15 @@
 import HomePage from '@/views/HomePage.vue';
+import ChatPage from '@/views/ChatPage.vue';
 
 export default [
     {
         path: '/',
         name: 'HomePage',
         component: HomePage,
+    },
+    {
+        path: '/chat',
+        name: 'ChatPage',
+        component: ChatPage,
     },
 ];
