06c112d8da1e3286441bc7821db142bed4f6010d
JaeeunCho <redjoun@gmail.com>
2024-07-26 15:29:46 +0900

    Feat chat and auction form

--- src/components/AuctionFormCompo.vue
+++ src/components/AuctionFormCompo.vue
@@ -1,43 +1,84 @@
 <template>
-  <form @submit="handleRegisterAuction">
-    상품명<input type="text" v-model="newAuction.itemName" required>
-    카테고리 
-    <select name="category" v-model="newAuction.categoryId" required> 
-        <option value="1">패션</option>
-        <option value="2">가전</option>
-        <option value="3">스포츠</option>
-        <option value="4">수집품</option>
-        <option value="5">기타</option>
-    </select>
-    상품 상태
-    <select name="auctionProductStatus" v-model="newAuction.auctionProductStatusId" required>
-        <option value="1">중고</option>
-        <option value="2">새상품</option>
-        <option value="3">미개봉</option>
-    </select>
-    상품 위치
-    <input type="text" v-model="newAuction.location" required/>
-    시작 가격 
-    <input type="number" v-model="newAuction.startPrice" required/>
-    판매 기간
-    <input type="datetime-local" v-model="newAuction.startDate" :min="currentDateTime" @change="validateStartDate" required>
-    <input type="datetime-local" v-model="newAuction.dueDate" :min="startDateTime" @change="validateDueDate" required>
-    설명
-    <textarea v-model="newAuction.description" maxlength="3000" required></textarea>
-    <p>{{ newAuction.description.length }} / 3000</p>
-    사진 등록
-    <input type="file" ref="imageFiles" multiple @change="onFileChange" accept="image/*" required>
-    * 첫번째로 등록한 사진이 대표 사진입니다.
-    <div v-if="imagePreviews.length">
-      <div class="image-preview-container">
-        <div v-for="(image, index) in imagePreviews" :key="index" class="image-preview">
-          <button type="button" class="remove-button" @click="removeImage(index)">X</button>
-          <img :src="image" :alt="'미리보기 ' + (index + 1)" width="100" height="100">
-        </div>
-      </div>
-    </div>
-    <button type="submit" :disabled="isSubmitting">등록하기</button>
-  </form>
+  <div class="container">
+    <div class="maintitle aligncenter">상품 등록</div>
+
+      <form @submit="handleRegisterAuction">
+        <table>
+          <tr class="leftwidth">
+            <td>상품<span class="redcolor"> *</span></td>
+            <td><input type="text" class="inputform" v-model="newAuction.itemName" required></td>
+          </tr>
+          <tr>
+            <td>카테고리<span class="redcolor"> *</span></td>
+            <td>
+              <select name="category" class="inputform" v-model="newAuction.categoryId" required> 
+                <option value="1">패션</option>
+                <option value="2">가전</option>
+                <option value="3">스포츠</option>
+                <option value="4">수집품</option>
+                <option value="5">기타</option>
+              </select>
+            </td>
+          </tr>
+          <tr>
+            <td>상품 상태<span class="redcolor"> *</span></td>
+            <td>
+              <select name="auctionProductStatus" class="inputform" v-model="newAuction.auctionProductStatusId" required>
+                  <option value="1">중고</option>
+                  <option value="2">새상품</option>
+                  <option value="3">미개봉</option>
+              </select>
+            </td>
+          </tr>
+          <tr>
+            <td>상품 위치<span class="redcolor"> *</span></td>
+            <td><input type="text" class="inputform" v-model="newAuction.location" required/></td>
+          </tr>
+          <tr>
+            <td>시작 가격<span class="redcolor"> *</span></td>
+            <td><input type="number" class="inputform" v-model="newAuction.startPrice" required/></td>
+          </tr>
+          <tr>
+            <td>판매 기간<span class="redcolor"> *</span></td>
+            <td>
+            <input type="datetime-local" class="inputform" v-model="newAuction.startDate" :min="currentDateTime" @change="validateStartDate" required>
+            <input type="datetime-local" class="inputform" v-model="newAuction.dueDate" :min="startDateTime" @change="validateDueDate" required>
+            </td>
+          </tr>
+          <tr>
+            <td>설명<span class="redcolor"> *</span></td>
+            <td><textarea class="inputform" v-model="newAuction.description" maxlength="3000" required></textarea>
+            <span class="tinytitle alignright">{{ newAuction.description.length }} / 3000</span></td>
+          </tr>
+          <tr>
+            <td>사진 등록<span class="redcolor"> *</span></td>
+            <td><input type="file" class="inputform" ref="imageFiles" multiple @change="onFileChange" accept="image/*" required>
+            <span class="tinytitle">첫번째로 등록한 사진이 대표 사진입니다.</span></td>
+          </tr>
+          <tr>
+            <td>
+            </td>
+            <td>
+              <div v-if="imagePreviews.length">
+                <div class="image-preview-container">
+                  <div v-for="(image, index) in imagePreviews" :key="index" class="image-preview">
+                    <button type="button" class="remove-button" @click="removeImage(index)">X</button>
+                    <img :src="image" :alt="'미리보기 ' + (index + 1)" width="100" height="100">
+                  </div>
+                </div>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="2">
+              <center>
+                <button type="submit" class="actionbutton" :disabled="isSubmitting">등록하기</button>
+              </center>
+            </td>
+          </tr>
+        </table>
+      </form>
+  </div>
 </template>
 
 <script>
@@ -178,25 +219,55 @@ export default {
 </script>
 
 <style scoped>
+@import '@/assets/style.css';
+
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+tr, td {
+  padding: 10px 20px;
+  border: none !important;
+}
+
+.leftwidth {
+  width: 250px;
+}
+
+.inputform {
+  width: 100%;
+  border: 1px #999999 solid;
+  border-radius: 10px;
+  padding: 10px;
+}
+
+.actionbutton {
+  width: 400px;
+}
+
 input, select, textarea, button {
   display: block;
   margin-bottom: 10px;
 }
+
 .image-preview-container {
   display: flex;
   flex-wrap: wrap;
 }
+
 .image-preview {
   position: relative;
   display: inline-block;
   margin: 10px;
 }
+
 .remove-button {
   position: absolute;
   top: 5px;
   right: 5px;
   background: white; 
-  border: 2px solid black;
+  border: 1px solid black;
   border-radius: 50%;
   width: 20px;
   height: 20px;
@@ -204,5 +275,28 @@ input, select, textarea, button {
   align-items: center;
   justify-content: center;
   cursor: pointer;
+}
+
+/* 파일 입력 필드의 기본 텍스트를 숨깁니다 */
+input[type="file"]::file-selector-button {
+  content: '파일 선택';
+}
+
+input[type="file"] {
+  color: transparent; /* 기본 텍스트 색상을 투명하게 설정 */
+}
+
+input[type="file"]:before {
+  content: '파일 선택';
+  display: inline-block;
+  background-color: #EEEEEE;
+  color: #000000;
+  padding: 10px;
+  border-radius: 10px;
+  cursor: pointer; 
+}
+
+input[type="file"]::-webkit-file-upload-button {
+  visibility: hidden;
 }
 </style>
