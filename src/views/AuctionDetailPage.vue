1f0b9fc25c7d40ddfb71abc7781d56e824ae7c8e
YunBins <kig0632@naver.com>
2024-07-29 22:49:22 +0900

    Feat:bid auction
    Feat:bid auction

--- src/views/AuctionDetailPage.vue
+++ src/views/AuctionDetailPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container" v-if="isDataLoaded">
+  <div class="container" v-if="isAuctionDataLoaded && isBiddingDataLoaded">
     <div class="tinytitle">카테고리 > {{ auctionData.categoryName }} </div>
     <div class="maintitle spacebetween">
       <span>{{ auctionData.itemName }}
@@ -47,7 +47,7 @@
       </tr>
       <tr>
         <td class="aligncenter">현재가격</td>
-        <td class="alignright">1000원</td>
+        <td class="alignright">{{ biddingData[0].price }}원</td>
       </tr>
       <tr>
         <td class="aligncenter">판매기간</td>
@@ -56,11 +56,42 @@
       </tr>
       <tr>
         <td class="aligncenter">입찰상태</td>
-        <td class="alignright">23 bids</td>
+        <td class="alignright">{{ biddingData.length }} bids</td>
       </tr>
       <tr>
         <td class="aligncenter" colspan="2">
-          <v-btn class="actionbutton" type="button" @click="requestBid">입찰하기</v-btn>
+            <v-dialog v-model="dialog"  max-width="500">
+              <template v-slot:activator="{ on }">
+                <v-btn class="actionbutton" dark v-on="on" @click.stop="dialog = true">입찰하기</v-btn>
+              </template>
+              <v-card>
+                <v-card-title class="headline">입찰 신청</v-card-title>
+                <v-card-text>
+                  <v-layout row wrap>
+                        <v-flex xs12 sm6>
+                          <p class="text-lg-h6 font-weight-bold">현재 입찰가</p>
+                        </v-flex>
+                        <v-flex xs12 sm6>
+                          <p class="text-lg-h6 font-weight-bold">{{ biddingData[0].price }}원</p>
+                        </v-flex>
+                        <v-flex xs12 sm6>
+                          <p class="text-lg-h6 font-weight-bold">입찰가</p>
+                        </v-flex>
+                        <v-flex xs12 sm6>
+                          <v-text-field
+                            single-line
+                            outline v-model="newBidingPrice"
+                            :error-messages="errorMessages"
+                            @input="validateBidPrice"></v-text-field>
+                        </v-flex> 
+                  </v-layout>
+                </v-card-text>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="dark" flat @click="submitBid()">입찰 하기</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
         </td>
       </tr>
       <tr>
@@ -80,14 +111,35 @@
       <tr>
         <td colspan="3">
           <div v-if="selectedMenu === 'description'">{{ auctionData.description }}</div>
-          <div v-if="selectedMenu === 'bidHistory'">{{ bidHistory }}</div>
+          <div v-if="selectedMenu === 'bidHistory'">
+            <div v-for = "(bidding, index) in biddingData" :key="index">
+              <span> {{ index+1 }}
+              {{ bidding.price }}
+              {{ bidding.memberId}}
+              {{ bidding.time }}</span>
+            </div>
+          </div>
         </td>
       </tr>
     </table>
+    <!-- 성공 메시지 모달 -->
+    <v-dialog v-model="successDialog" max-width="500">
+      <v-card>
+        <v-card-title class="headline">입찰 완료</v-card-title>
+        <v-card-text>
+          입찰이 성공적으로 완료되었습니다.
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="dark" flat @click="successDialog = false">확인</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
   <div v-else>
     <p>Loading...</p>
   </div>
+
 </template>
 
 <script>
@@ -111,12 +163,19 @@ export default {
         dueDate: '',
         description: '',
       },
+      biddingData: [],
       images: [],
       mainImageIndex: null,
-      isDataLoaded: false,
+      isAuctionDataLoaded: false,
+      isBiddingDataLoaded: false,
       selectedMenu: 'description',
       bidHistory:"",
       remainingTime: "",
+      dialog : false,
+      newBidingPrice:null,
+      successDialog: false,
+      errorMessages: [],
+      isBidPriceValid: false,
     }
   },
   methods: {
@@ -134,12 +193,68 @@ export default {
           this.auctionData = res.data.auctionData;
           this.images = res.data.images;
           this.mainImageIndex = res.data.mainImageIndex;
-          this.isDataLoaded = true; // 데이터 로드 완료
+          this.isAuctionDataLoaded = true; // 데이터 로드 완료
         } else {
           console.log(res.statusText);
         }
       } catch (error) {
         console.error('Error:', error);
+      }
+    },
+    async setBiddingData() {
+      try {
+        const res = await axios.get(`/auctions/${this.$route.params.id}/bids`, {
+          headers: {
+            withCredentials: true,
+          },
+        });
+
+        if (res.status === 200) {
+          console.log(res.data);
+          this.biddingData = res.data.biddingData;
+          this.isBiddingDataLoaded = true; // 데이터 로드 완료
+        } else {
+          console.log(res.statusText);
+        }
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    },
+    async submitBid(){
+      if (!this.isBidPriceValid) {
+        return; // 유효하지 않은 경우, 제출하지 않음
+      }
+
+      const formData = new FormData();
+      formData.append("price", this.newBidingPrice);
+
+      try {
+        const res = await axios.post(`/auctions/${this.$route.params.id}/bids`, formData, {
+          headers: {
+            withCredentials: true,
+          },
+        });
+
+        if (res.status === 200) {
+          console.log(res.data);
+          this.successDialog = true; 
+          this.dialog = false; 
+          this.setBiddingData(); // 입찰 데이터 새로고침
+
+        } else {
+          console.log(res.statusText);
+        }
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    },
+    validateBidPrice() {
+      this.errorMessages = [];
+      this.isBidPriceValid = true;
+
+      if (this.newBidingPrice <= this.biddingData[0].price) {
+        this.errorMessages.push(`입찰가는 현재 입찰가(${this.biddingData[0].price})보다 커야 합니다.`);
+        this.isBidPriceValid = false;
       }
     },
      updateRemainingTime() {
@@ -160,9 +275,6 @@ export default {
     stopTimer() {
       clearInterval(this.timer);
     },
-    requestBid(){
-      
-    },
     showBidHistory(){
       this.selectedMenu = 'bidHistory';
     },
@@ -172,6 +284,7 @@ export default {
   },
   mounted() {
     this.setAuctionData();
+    this.setBiddingData();
     this.updateRemainingTime();
     this.startTimer();
   },
