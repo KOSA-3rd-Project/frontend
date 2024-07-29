74df3d5af7f2cede32f20091e3fadf920cccb6ef
JaeeunCho <redjoun@gmail.com>
2024-07-29 12:15:02 +0900

    Feat payment confirm page

--- src/views/PaymentPage.vue
+++ src/views/PaymentPage.vue
@@ -1,86 +1,34 @@
 <template>
   <div class="container">
     <div class="maintitle aligncenter">결제하기</div>
-    <div class="middletitle">상품정보</div>
-    <div>
-      <table>
-        <tr>
-          <td class="imgcell" rowspan="5">
-            <img class="productimg" src="@/assets/00.jpg">
-          </td>
-          <td>품목명</td>
-          <td class="alignright">가격</td>
-        </tr>
-        <tr>
-          <td colspan="2">카테고리</td>
-        </tr>
-        <tr>
-          <td colspan="2">&nbsp;</td>
-        </tr>
-        <tr>
-          <td colspan="2">경매진행일자</td>
-        </tr>
-      </table>
-
-      <hr class="line"/>
-
-      <div class="middletitle">배송정보</div>
-      <form @submit.prevent="submitForm">
-        <table>
-          <tr class="leftwidth">
-            <td><label for="name">이름</label></td>
-            <td><input type="text" id="name" v-model="form.name" class="form-control inputform" placeholder="이름을 입력해주세요" required></td>
-          </tr>
-          <!-- <tr>
-            <td><label for="email">이메일</label></td>
-            <td class="alignright"><input type="email" id="email" v-model="form.email" class="form-control" placeholder="Your Email" required></td>
-          </tr> -->
-          <tr>
-            <td><label for="phone">전화번호</label></td>
-            <td><input type="tel" id="phone" v-model="form.phone" class="form-control inputform" placeholder="전화번호를 입력해주세요" required></td>
-          </tr>
-          <tr>
-            <td><label for="address">배송지</label></td>
-            <td><input type="text" id="address" v-model="form.address" class="form-control inputform" placeholder="주소를 입력해주세요" required></td>
-          </tr>
-          <tr>
-            <td><label for="message">배송메세지</label></td>
-            <td><input type="text" id="message" v-model="form.message" class="form-control inputform" placeholder="메세지를 입력해주세요"></td>
-          </tr>
-        </table>
-
-        <hr class="line"/>
-
-        <div class="middletitle">결제정보</div>
-        <table>
-          <tr class="leftwidth">
-            <td>상품</td>
-            <td class="alignright">{{ totalprice }}원</td>
-          </tr>
-          <tr>
-            <td>배송비</td>
-            <td class="alignright">3000원</td>
-          </tr>
-          <tr>
-            <td>최종 결제 금액</td>
-            <td class="alignright">{{ totalprice + 3000 }}원</td>
-          </tr>
-        </table>
-        <div class="actioncell">
-          <v-btn class="actionbutton" type="button" @click="requestPay">결제하기</v-btn>
-        </div>
-      </form>
+    <ProductInfo :price="price" imageSrc="@/assets/00.jpg" />
+    <hr class="line"/>
+    <ShippingInfoForm :form.sync="form" />
+    <hr class="line"/>
+    <PaymentInfo :price="price" :shippingFee="shippingFee" :totalprice="totalprice" />
+    <div class="actioncell">
+      <v-btn class="actionbutton" type="submit" @click="submitForm">결제하기</v-btn>
     </div>
   </div>
 </template>
 
 <script>
+import ProductInfo from '@/components/payment/ProductInfo.vue';
+import ShippingInfoForm from '@/components/payment/ShippingInfoForm.vue';
+import PaymentInfo from '@/components/payment/PaymentInfo.vue';
+
 export default {
   name: 'PaymentPage',
+  components: {
+    ProductInfo,
+    ShippingInfoForm,
+    PaymentInfo
+  },
   data() {
     return {
       merchantUid: "order-" + new Date().getTime(),
-      totalprice: 1000,
+      price: 100,
+      shippingFee: 300,
       form: {
         name: '',
         phone: '',
@@ -89,104 +37,89 @@ export default {
       },
     };
   },
-
+  computed: {
+    totalprice() {
+      return this.price + this.shippingFee;
+    }
+  },
   methods: {
-    requestPay() {
+    submitForm() {
       const { merchantUid, totalprice, form } = this;
 
       window.IMP.init("imp30164472");
 
       window.IMP.request_pay({
-          pg: "html5_inicis",
-          pay_method: "card",
-          merchant_uid: merchantUid,
-          name: "Drop the bid",
-          amount: totalprice,
-          buyer_email: "redjoun@gmail.com",
-          buyer_name: form.name,
-          buyer_tel: form.phone,
-          buyer_addr: form.address,
-      }, function (rsp) {
-          if (rsp.success) {
-              // 결제 성공 시
-              console.log(rsp);
-              alert('결제가 완료되었습니다.');
-              
-              // 결제 성공 후 서버에 결제 완료 여부와 결제 시간 업데이트 요청
-              fetch(`/payment`, {
-                  method: 'POST',
-                  headers: {
-                      'Content-Type': 'application/json'
-                  },
-                  body: JSON.stringify({
-                      paidat: new Date().toISOString()
-                  })
-              })
-              .then(response => {
-                  if (response.ok) {
-                      alert('결제 정보가 성공적으로 업데이트되었습니다.');
-                      location.reload();
-                  } else {
-                      alert('결제 정보 업데이트에 실패했습니다.');
-                  }
-              })
-              .catch(error => {
-                  console.error('Error:', error);
-                  alert('결제 정보 업데이트 중 오류가 발생했습니다.');
-              });
+        pg: "html5_inicis",
+        pay_method: "card",
+        merchant_uid: merchantUid,
+        name: "Drop the bid",
+        amount: totalprice,
+        buyer_email: "redjoun@gmail.com",
+        buyer_name: form.name,
+        buyer_tel: form.phone,
+        buyer_addr: form.address,
+      }, (rsp) => {
+        if (rsp.success) {
+          // 결제 성공 시
+          console.log(rsp);
+          alert('결제가 완료되었습니다.');
 
-          } else {
-              // 결제 실패 시
-              console.log(rsp);
-              alert('결제에 실패하였습니다. 에러 내용: ' + rsp.error_msg);
-          }
-      })
+          // Save form data to localStorage
+          localStorage.setItem('formData', JSON.stringify(form));
 
-    },
+          // Send payment completion request to server
+          fetch(`/payment`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              auctionId: 1,
+              biddingSuccessId: 1,
+              name: form.name,
+              phoneNumber: form.phone,
+              address: form.address,
+              message: form.message,
+              createdAt: new Date().toISOString()
+            })
+          })
+          .then(response => {
+            if (response.ok) {
+              // Redirect to the status page
+              window.location.href = '/auction/paymentstatus';
+            } else {
+              alert('결제 정보 업데이트에 실패했습니다.');
+            }
+          })
+          .catch(error => {
+            console.error('Error:', error);
+            alert('결제 정보 업데이트 중 오류가 발생했습니다.');
+          });
+
+        } else {
+          // 결제 실패 시
+          console.log(rsp);
+          alert('결제에 실패하였습니다. 에러 내용: ' + rsp.error_msg);
+        }
+      });
+    }
   }
 }
 </script>
 
-
-<style>
+<style scoped>
 @import '@/assets/style.css';
 
-table {
-  width: 100%;
-  border-collapse: collapse;
-}
-
-tr, td {
-  padding: 10px 20px;
-  border: none !important;
-}
-
-.leftwidth {
-  width: 250px;
-}
-
-.inputform {
-  width: 100%;
-  border: 1px #999999 solid;
-  border-radius: 10px;
-  padding: 10px;
-}
-
-.imgcell {
-  width: 250px;
-  text-align: center;
-}
-
-.productimg {
-  width: 200px !important;
-  height: 200px !important;
-}
-
 .actionbutton {
+  margin: 10px;
   width: 400px;
 }
 
 .actioncell {
   text-align: center;
+}
+
+.line {
+  border: 1px solid #ddd;
 }
 </style>
